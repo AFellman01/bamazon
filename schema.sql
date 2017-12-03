1ef43d27cdc1ec05583e6d3a4d486b2601d8866b
Alexander Fellman <alexanderfellman@Alexanders-MBP.home>
2017-12-03 12:21:47 -0500

    SQL

--- schema.sql
+++ schema.sql
@@ -12,4 +12,8 @@ CREATE TABLE products (
   PRIMARY KEY (item_id)
 );
 
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Gutenberg Bible", "Books", 15000, 2), ("Nintendo Switch", "Electronics", 290, 120), ("Spirited Away", "Entertainment", 15, 200), ("Demons don't Dream", "Books", 8, 150), ("Sega Dreamcast", "Electronics", 190, 30), ("Batman", "Entertainment", 6, 190)
+
 SELECT * FROM products;
